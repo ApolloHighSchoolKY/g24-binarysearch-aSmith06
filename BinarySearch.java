0519bf9db5be5f05f6af801f34c6b65d2eb0de98
aSmith06 <andrew.smith2@stu.daviess.kyschools.us>
2025-04-16 14:08:50 +0000

    Binaries have been examined

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,6 +4,25 @@ public class BinarySearch
 {
 	public static int binarySearch(int[] ray, int item)
 	{
+		Arrays.sort(ray);
+		//System.out.println("After calling binarySearch");
+		//System.out.println(Arrays.toString(ray));
 
+		int bottom = 0;
+		int top = ray.length-1;
+		int middle = (top + bottom)/2;
+		
+		while(top >= bottom)
+		{
+			if(ray[middle] < item)
+				bottom = middle + 1;
+			else if(ray[middle] > item)
+				top = middle - 1;
+			else if(ray[middle] == item)
+				return middle;
+
+			middle = (top + bottom)/2;
+		}
+		return -1;
 	}
 }
